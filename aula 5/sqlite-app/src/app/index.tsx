ebcdceebe81e4f9abacf8349762ba92ce96ed7cb
wmvj <wmvj@unifesspa.edu.br>
2025-01-18 21:15:13 -0300

    Operação de Criação, Exclusão, Atualização e Listagem

--- aula 5/sqlite-app/src/app/index.tsx
+++ aula 5/sqlite-app/src/app/index.tsx
@@ -1,23 +1,27 @@
-import {View, Button, Alert, Text} from 'react-native';
+import {View, Button, Alert, FlatList} from 'react-native';
 import {router} from 'expo-router';
-import {useState} from 'react';
-import {Input} from "@/components/Input"
+import {useEffect, useState} from 'react';
 
-import {useProductDatabase} from "@/database/useProductDatabase";
+import {Input} from "@/components/Input"
+import {Product} from "@/components/Product"
+
+import {useProductDatabase, ProductDatabase} from "@/database/useProductDatabase";
 
 export default function Index() {
 
     const [id, setId] = useState("")
     const [name, setName] = useState("")
     const [quantity, setQuantity] = useState("")
-    const [products, setProducts] = useState([])
+    // const [products, setProducts] = useState([])
+    const [search, setSearch] = useState("")
+    const [products, setProducts] = useState<ProductDatabase[]>([])
 
     const productDatabase = useProductDatabase()
 
     async function create() {
         try {
             if (isNaN(Number(quantity))) {
-                return Alert.alert('Quantidade precisa ser um número!')
+                return Alert.alert('A quantidade precisa ser um número!')
             }
 
             const response = await productDatabase.create({
@@ -31,7 +35,65 @@ export default function Index() {
             console.log(error)
         }
     }
+    
+    async function update() {
+        try {
+          if (isNaN(Number(quantity))) {
+            return Alert.alert("Quantidade", "A quantidade precisa ser um número!")
+          }
+    
+          const response = await productDatabase.update({
+            id: Number(id),
+            name,
+            quantity: Number(quantity),
+          })
+    
+          Alert.alert("Produto atualizado!")
+        } catch (error) {
+          console.log(error)
+        }
+    }
 
+    async function list(){
+        try {
+            const response = await productDatabase.searchByName(search)
+            setProducts(response)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function remove(id: number){
+        try {
+            await productDatabase.remove(id)
+            await list()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    function details(item: ProductDatabase) {
+        setId(String(item.id))
+        setName(item.name)
+        setQuantity(String(item.quantity))
+    }
+
+    async function handleSave(){
+        if(id){
+            update()
+        } else{
+            create()
+        }
+
+        setId("")
+        setName("")
+        setQuantity("")
+        await list()
+    }
+
+    useEffect(() => {
+        list()
+    }, [search])
 
     return (
         <View style={{flex: 1, justifyContent: 'center', padding: 32, gap: 16}}>
@@ -46,7 +108,22 @@ export default function Index() {
                 value={quantity}
             />
 
-            <Button title='Salvar' onPress={create} />
+            <Button title='Salvar' onPress={handleSave} />
+
+            <Input placeholder='Pesquisar' onChangeText={setSearch} />
+
+            <FlatList
+                data={products}
+                keyExtractor={(item) => String(item.id)}
+                renderItem={({item}) => (
+                    <Product
+                        data={item}
+                        onPress={() => details(item)}
+                        onDelete={() => remove(item.id)}
+                    />
+                )}
+                contentContainerStyle={{gap: 16}}
+            />
 
         </View>
     )
--- aula 5/sqlite-app/src/database/useProductDatabase.ts
+++ aula 5/sqlite-app/src/database/useProductDatabase.ts
@@ -26,9 +26,48 @@ export function useProductDatabase(){
         } catch (error) {
             throw error
         } finally{
+            await statement.finalizeAsync()        
+        }
+    }
+
+    async function update(data: ProductDatabase){
+        const statement = await database.prepareAsync(
+            "UPDATE products SET name = $name, quantity = $quantity WHERE id = $id"
+        )
+
+        try {
+            await statement.executeAsync({
+                $id: data.id,
+                $name: data.name,
+                $quantity: data.quantity,
+            })
+
+        } catch (error) {
+            throw error
+        } finally{
             await statement.finalizeAsync()        }
     }
 
+    async function searchByName(name: string){
+        try {
+            const query = "SELECT * FROM products WHERE name LIKE ?"
+            const response = await database.getAllAsync<ProductDatabase>(
+                query,
+                `%${name}%`
+            )
+            return response
+        } catch (error) {
+            throw error
+        }
+    }
 
-    return {create}
+    async function remove(id: number){
+        try {
+            await database.execAsync("DELETE FROM products WHERE id = " + id)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    return {create, searchByName, remove, update}
 }
