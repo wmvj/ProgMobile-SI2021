3072e291d9732f25e8e714cf98a49a31a2f53b6e
wmvj <wmvj@unifesspa.edu.br>
2025-01-23 16:25:26 -0300

    Botão de autenticação

--- aula 6/auth-app/src/app/index.tsx
+++ aula 6/auth-app/src/app/index.tsx
@@ -1,10 +1,26 @@
+import { Button } from '@/components/Button';
 import {View, Text, StyleSheet, Linking } from 'react-native';
 
 export default function SignIn() {
+
+    async function signInWithGoogle(){
+        try{
+            await Linking.openURL('https://www.google.com')
+        }catch(error){
+            console.log(error)
+        }
+    }
+
   return (
 
    <View style={styles.container}> 
       <Text style={styles.text}>Entrar</Text>
+      <Button
+        icon='logo-google'
+        title='Entrar com o Google'
+        onPress={signInWithGoogle}
+        isLoading={false}
+      />
 
     </View>
   );
