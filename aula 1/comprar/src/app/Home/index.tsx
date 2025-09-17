61733ec0bd8505b4184f902c97bfd086f4b20032
wmvj <wmvj@unifesspa.edu.br>
2025-09-16 23:28:45 -0300

    Add hooks e localStorage

--- aula 1/comprar/src/app/Home/index.tsx
+++ aula 1/comprar/src/app/Home/index.tsx
@@ -1,57 +1,86 @@
-import { Text, View, Image, TouchableOpacity, FlatList } from "react-native"
+import { Alert, FlatList, Image, Text, TouchableOpacity, View } from "react-native"
 
 import { styles } from "./styles"
-
 import { Button } from "@/components/Button"
 import { Input } from "@/components/Input"
 import { Filter } from "@/components/Filter"
 import { FilterStatus } from "@/types/FilterStatus"
 import { Item } from "@/components/Item"
-import { useState } from "react"
-
-
+import { useEffect, useState } from "react"
+import { itemsStorage, ItemsStorage } from "@/storage/itemsStorage"
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
 
-const ITEMS = [
-  {
-    id: "1",
-    status: FilterStatus.DONE,
-    description: "1 pacote de café"
-  },
-  {
-    id: "2",
-    status: FilterStatus.PENDING,
-    description: "3 pacotes de macarrão"
-  },
-  {
-    id: "3",
-    status: FilterStatus.PENDING,
-    description: "3 cebolas"
-  },
-]
-
 export function Home() {
-
   const [filter, setFilter] = useState(FilterStatus.PENDING)
+  const [description, setDescription] = useState("")
+  const [items, setItems] = useState<ItemsStorage[]>([])
+
+  async function handleAdd() {
+    if (!description.trim()) {
+      return Alert.alert("Adicionar", "Informe a descrição para adicionar.")
+    }
+
+    const newItem = {
+      id: Math.random().toString().substring(2),
+      description,
+      status: FilterStatus.PENDING,
+    }
+
+    await itemsStorage.add(newItem)
+    await itemsByStatus()
+
+    Alert.alert("Adicionado", `Adicionado ${description}`)
+    setFilter(FilterStatus.PENDING)
+    setDescription("")
+  }
+
+  async function itemsByStatus() {
+    try {
+      const response = await itemsStorage.getByStatus(filter)
+      setItems(response)
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Erro", "Não foi possível filtrar os itens.")
+    }
+  }
+
+  async function handleRemove(id: string) {
+    try {
+      await itemsStorage.remove(id)
+      await itemsByStatus()
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Remover", "Não foi possível remover o item.")
+    }
+  }
+
+  useEffect(() => {
+    itemsByStatus()
+  }, [filter])
 
   return (
     <View style={styles.container}>
-      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+      <Image source={require('@/assets/logo.png')} style={styles.logo} />
 
-      <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar?" />
-        <Button title="Entrar" />
+      <View style={styles.form}
+      >
+        <Input 
+          placeholder="O que você precisa comprar?" 
+          onChangeText={setDescription}
+          value={description}
+        />
+        <Button title="Adicionar" onPress={handleAdd} />
       </View>
 
       <View style={styles.content}>
         <View style={styles.header}>
           {FILTER_STATUS.map((status) => (
             <Filter
-              key={status}
-              status={status}
-              isActive={filter === status}
-              onPress={() => setFilter(status)}
+               key={status}
+               status={status} 
+               isActive={filter === status}
+               onPress={() => setFilter(status)} 
             />
           ))}
 
@@ -60,14 +89,14 @@ export function Home() {
           </TouchableOpacity>
         </View>
 
-        <FlatList
-          data={ITEMS}
+        <FlatList 
+          data={items}
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
             <Item
-              data={item}
+              data={item} 
               onStatus={() => console.log("mudar o status")}
-              onRemove={() => console.log("remover")}
+              onRemove={() => handleRemove(item.id)}
             />
           )}
           showsVerticalScrollIndicator={false}
